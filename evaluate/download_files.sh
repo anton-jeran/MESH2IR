3033e9484c66b07b9d9bf2d3098de6f22d9fdda2
Anton Jeran Ratnarajah <73730558+anton-jeran@users.noreply.github.com>
2022-04-28 23:20:47 -0400

    Add files via upload

---
+++ evaluate/download_files.sh
@@ -0,0 +1,7 @@
+gdown https://drive.google.com/uc?id=1poy8LRN9Wakz-rGhatdL20Hv3FGKHSD4
+unzip Models.zip
+gdown https://drive.google.com/uc?id=1rMzur3QtSGMVVW0kxBVb7a23Mdd0WtLM
+unzip Meshes.zip
+gdown https://drive.google.com/uc?id=1LUmJFvmJnBDfbXkZYcXR7HeRO8sA_VZg
+unzip Paths.zip
+rm -rf Models.zip Meshes.zip Paths.zip
